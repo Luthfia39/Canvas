b9e6952e00da69faa8b4d17f646505ba49f5d4e9
Luthfia39 <you@example.com>
2023-05-03 14:24:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.canvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint (Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+
+    private Rect mRect = new Rect();
+    private Rect mBounds = new Rect();
+//    private static final int OFFSET = 120;
+    private static final int OFFSET = 80;
+    private int mOffset = OFFSET;
+    private static final int MULTIPLIER = 100;
+
+    private int mColorBg;
+    private int mColorRectangle;
+    private int mColorCircle;
+    private int mColorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize based on XML
+        mColorBg = ResourcesCompat.getColor(getResources(), R.color.colorBackground, null);
+        mColorRectangle = ResourcesCompat.getColor(getResources(), R.color.colorRectangle, null);
+        mColorCircle = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
+        mColorText = ResourcesCompat.getColor(getResources(), R.color.black, null);
+
+        // warna
+        mPaint.setColor(mColorBg);
+        mPaintText.setColor(mColorText);
+        mPaintText.setTextSize(70);
+
+        mImageView = findViewById(R.id.my_image_view);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawSomething(v);
+            }
+        });
+    }
+
+    private void drawSomething(View view) {
+        int vWidth = view.getWidth();
+        int vHeight = view.getHeight();
+        int halfWidth = vWidth/2;
+        int halfHeight = vHeight/2;
+
+        if (mOffset == OFFSET){
+            mBitmap = Bitmap.createBitmap(vWidth, vHeight, Bitmap.Config.ARGB_8888);
+            mImageView.setImageBitmap(mBitmap);
+            mCanvas = new Canvas(mBitmap);
+            mCanvas.drawColor(mColorBg);
+
+            mCanvas.drawText(getString(R.string.keep_tapping), 100, 100, mPaintText);
+            mOffset += OFFSET;
+        }else{
+            // border/batasan
+            if (mOffset < halfWidth && mOffset < halfHeight) {
+                // change color
+                mPaint.setColor(mColorRectangle - MULTIPLIER * mOffset);
+                // draw more than 1 rectangle with different size (more small)
+                mRect.set(
+                        mOffset, // left
+                        mOffset, // top
+                        vWidth - mOffset, // right
+                        vHeight - mOffset // bottom
+                );
+                mCanvas.drawRect(mRect, mPaint);
+                mOffset += OFFSET;
+                Log.d("Coba Canvas", String.valueOf(mOffset));
+            }
+            // jika mentok, maka akan muncul gambar lingkaran
+            else {
+                mPaint.setColor(mColorCircle);
+                mCanvas.drawCircle(halfWidth, halfHeight, halfHeight/3, mPaint);
+                String text = getString(R.string.done);
+
+                mPaintText.getTextBounds(text, 0, text.length(), mBounds);
+                int x = halfWidth - mBounds.centerX();
+                int y = halfHeight - mBounds.centerY();
+                mCanvas.drawText(text, x, y, mPaintText);
+            }
+        }
+        view.invalidate();
+    }
+}
